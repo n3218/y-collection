86ea51b523db21af93881145894103f49c86a1b4
Natalia Shport <shport.natalia@gmail.com>
2020-11-30 23:19:53 -0800

    Simple UI structure done, and added server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,29 @@
-import "./App.css"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import HomeScreen from "./screens/HomeScreen"
+import AboutScreen from "./screens/AboutScreen"
+import HowToScreen from "./screens/HowToScreen"
+import ProductScreen from "./screens/ProductScreen"
+import Switch from "react-bootstrap/esm/Switch"
+
 const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome to Woolunatics</h1>
-    </div>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Switch>
+            <Route path="/" exact component={HomeScreen} />
+            <Route path="/about" component={AboutScreen} />
+            <Route path="/how-to" component={HowToScreen} />
+            <Route path="/products/:id" component={ProductScreen} />
+          </Switch>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   )
 }
 
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,62 @@
+import React from "react"
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button, Container } from "react-bootstrap"
+// import { ReactComponent as Logo } from "../assets/wool.svg"
+import { LinkContainer } from "react-router-bootstrap"
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="light" expand="lg" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img className="logo" alt="Logo" src="/assets/wool.svg" />
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <LinkContainer to="/about">
+                <Nav.Link>About</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/how-to">
+                <Nav.Link>How to Order</Nav.Link>
+              </LinkContainer>
+              <NavDropdown title="Catalog" id="basic-nav-dropdown">
+                <LinkContainer to="/cashmere">
+                  <NavDropdown.Item>Cachmere</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/mohair">
+                  <NavDropdown.Item>Mohair</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/cotton">
+                  <NavDropdown.Item>Cotton</NavDropdown.Item>
+                </LinkContainer>
+                <NavDropdown.Divider />
+                <LinkContainer to="/all">
+                  <NavDropdown.Item>All</NavDropdown.Item>
+                </LinkContainer>
+              </NavDropdown>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/sign-in">
+                <Nav.Link>
+                  <i className="fas fa-user"></i> Sign In
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="warning">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
