e5902854d730c40a4c4bba57de687c248e52317f
Natalia Shport <shport.natalia@gmail.com>
2020-12-01 00:47:16 -0800

    data moved to backend, and DB created

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,20 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Col, ListGroup, Row, Image, Card, Button } from "react-bootstrap"
 import { Link } from "react-router-dom"
-import products from "../products"
 import Rating from "../components/Rating"
+import axios from "axios"
 
 const ProductScreen = ({ match }) => {
-  const product = products.find(p => p._id === match.params.id)
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
+      setProduct(data)
+    }
+    fetchProduct()
+  }, [match])
+
   return (
     <>
       <Link to="/" className="btn btn-light my-3">
@@ -33,7 +42,7 @@ const ProductScreen = ({ match }) => {
               <ListGroup.Item>
                 <Row>
                   <Col>Price: </Col>
-                  <Col>{product.price}</Col>
+                  <Col>${product.price}</Col>
                 </Row>
               </ListGroup.Item>
 
